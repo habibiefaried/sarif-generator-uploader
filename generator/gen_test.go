db7fe5066934db4605296c91071052d337660951
Habibie Faried <habibiefaried@gmail.com>
2022-03-15 22:47:57 +0400

    feat: add tests

--- generator/gen_test.go
+++ generator/gen_test.go
@@ -7,9 +7,14 @@ import (
 
 func TestStructure(t *testing.T) {
 	m := Init()
-	m.AddDriverName("BurpSuiteBisa9")
+	m.AddDriverName("BurpSuiteBisa15")
 
 	err := m.AddRule("no-unused-vars", "disallow unused variables", "-", "Variables")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = m.AddRule("sql-injection", "SQL Injection", "-", "Variables")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -17,6 +22,23 @@ func TestStructure(t *testing.T) {
 	err = m.AddResults("no-unused-vars", "warning", "'x' is assigned a value but never used.", []string{"/http/api/user/2"})
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	err = m.AddResults("sql-injection", "error", "SQL Injection in this URL", []string{"/http/api/sqli/14"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(m.Runs) != 1 {
+		t.Fatal("This tool only generates maximum 1 run, hardcoded just like that")
+	}
+
+	if len(m.Runs[0].Tool.Driver.Rules) != 2 {
+		t.Fatal("The rules should be 2")
+	}
+
+	if len(m.Runs[0].Results) != 2 {
+		t.Fatal("The rules should be 2")
 	}
 
 	j, err := m.GetJSON()
